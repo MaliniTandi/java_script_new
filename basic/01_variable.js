2d25aef47dcee64782dc749a251e518208bc6de3
Malini Tandi <94887805+MaliniTandi@users.noreply.github.com>
2024-06-02 18:18:17 +0000

    variable

---
+++ basic/01_variable.js
@@ -0,0 +1,8 @@
+const  accountId =1446
+let accountEmail = "malini@google.com"
+var accountPassword = "12345"
+accountcity = "Jamshedpur" 
+
+// accountId = 2
+console.log(accountId);
+console.table([accountEmail, accountId , accountPassword])
