aadaec5b10ef4bab07a0cc441d3c0c8ac6332a42
Malini Tandi <94887805+MaliniTandi@users.noreply.github.com>
2024-06-04 12:03:06 +0000

    comparision

--- basic/comparision.js
+++ basic/comparision.js
@@ -1,3 +1,18 @@
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >=0);
+
+// difference between == and ===
+ console.log("2"==2);       //only checks values
+console.log('2'===2);       // not only checks values but also the data types.
+
+
+let userone = {
+    email: "user@gmail.com",
+    upi: "userone@ybl"
+}
+
+let usrtwo = userone
+usrtwo.email = "malini@google.com"
+console.log(userone);
+console.log(usrtwo);
