dfcab8d042e77903ede97def37a8049a3a64d37d
Malini Tandi <94887805+MaliniTandi@users.noreply.github.com>
2024-06-05 18:13:13 +0000

    datetime

---
+++ basic/datetime.js
@@ -0,0 +1,17 @@
+// Dates
+/*let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleTimeString());
+console.log(typeof mydate);*/
+let mydate2 = new Date(2024, 6, 5)
+console.log(mydate2.toDateString());
+
+// let myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(mydate2.getTime());
+console.log(Math.floor(Date.now()/1000));
