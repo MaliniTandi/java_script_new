3349c4e1fbbabaaf0a9100a5a2d21aef3b5b14ca
Malini Tandi <94887805+MaliniTandi@users.noreply.github.com>
2024-06-23 17:03:10 +0000

    function

---
+++ function/basic01.js
@@ -0,0 +1,45 @@
+// function sayName(){
+//     console.log("M");
+//     console.log("A");
+//     console.log("L");
+//     console.log("I");
+//     console.log("N");
+//     console.log("I");
+// }
+// sayName()
+
+// function Add(num1 , num2){
+//     console.log(num1+num2);
+// }
+
+// Add(2,"d")
+
+// function loginmessage(username){
+//     return `${username} has logged in `
+// }
+
+// console.log(loginmessage("Shazmin Ali"));
+
+// function cal( val , val1 ,...num1)
+// {
+// return num1
+// }
+// console.log(cal(200,300,500,800,900));
+
+// const user = {
+//     username : "Shazmin",
+//     age :25
+
+// }
+//  function handleObject(anyobject){
+//     console.log(`username is ${anyobject.username} and age is ${anyobject.age}`);
+//  }
+//  handleObject(user);
+
+ const arr = [200, 300, 600, 500, 400]
+
+ function returnsvalue(getarray){
+    return getarray[2]
+
+ }
+ console.log(returnsvalue(arr));
